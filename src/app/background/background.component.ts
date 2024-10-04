7bf75fe3b14576172062921c5e8d9c68e5e37d0d
martin-gjorgjiev <martin.gorgiev@rocketmail.com>
2024-10-04 23:31:37 +0200

    initial look

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, effect, Renderer2} from '@angular/core';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-background',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './background.component.html',
   styleUrl: './background.component.css'
 })
 export class BackgroundComponent {
+  imgLink="https://i.imgur.com/xmdIVKv.jpeg";
 
 }
--- src/app/main-wrapper/main-wrapper.component.ts
+++ src/app/main-wrapper/main-wrapper.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CitySelectorComponent } from "../city-selector/city-selector.component";
+import { ClockComponent } from "../clock/clock.component";
+import { CurrentWeatherComponent } from "../current-weather/current-weather.component";
+import { FutureWeatherComponent } from "../future-weather/future-weather.component";
 
 @Component({
   selector: 'app-main-wrapper',
   standalone: true,
-  imports: [],
+  imports: [CitySelectorComponent, ClockComponent, CurrentWeatherComponent, FutureWeatherComponent],
   templateUrl: './main-wrapper.component.html',
   styleUrl: './main-wrapper.component.css'
 })
